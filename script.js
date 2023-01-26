4c4467a5e49ffc84a3938ddb00d486aef5e27792
Logan <logan.nelson012@gmail.com>
2023-01-25 16:21:15 -0800

    Created a function that will add a new book to the library.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 /* Book Storing Function */
 
-function Book(title, author, pages, read){
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.bookInfo = function() {
+        console.log(title, author, pages, read);
+    }
+}
+
+function addBookToLibrary() {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').checked
+    return new Book(title, author, pages, read);
 }
