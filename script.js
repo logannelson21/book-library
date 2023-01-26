c3c147d2e9288e9b139b26d1db147b1bb53bef3d
Logan <logan.nelson012@gmail.com>
2023-01-25 17:05:41 -0800

    Made a function that will push books into the new array. It is called addBookToLibrary.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-/* Book Storing Function */
+/* data structures */
 
-let myLibrary = [];
+let myLibrary;
+const DEFAULT_DATA = [{title: 'The Hobbit', author: 'Tolkien', isRead: 'read' }, {title: 'Meditations', author: 'Marcus Aurelius', isRead: 'read'}, {title: 'How to Change Your Mind', author: 'Micheal Pollan', isRead:'read'}];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.bookInfo = function() {
-        console.log(title, author, pages, read);
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const isRead = document.getElementById('isRead');
+const table = document.getElementById('table');
+const form = document.getElementById('form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    addBookToLibrary();
+
+})
+
+class Book {
+    constructor(name, author, isRead){
+        this.name = name;
+        this.author = author;
+        this.isRead = isRead;
     }
 }
 
+
+/* display function */
+
 function addBookToLibrary() {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const pages = document.getElementById('pages').value
-    const read = document.getElementById('read').checked
-    return new Book(title, author, pages, read);
+    if (title.value.length === 0 || author.value.length === 0) {
+        alert('Please fill all fields');
+        return;
+    }
+    const newBook = new Book(title.value, author.value, isRead.value);
+    myLibrary.push(newBook);
 }
