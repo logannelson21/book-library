23848694dd5395ec764e402842aba6a4cda0286e
Logan <logan.nelson012@gmail.com>
2023-01-26 18:55:22 -0800

    Got stuck so I deleted a few functions and will be trying again with a new approach.

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 /* data structures */
 
-let myLibrary;
-const DEFAULT_DATA = [{title: 'The Hobbit', author: 'Tolkien', isRead: 'read' }, {title: 'Meditations', author: 'Marcus Aurelius', isRead: 'read'}, {title: 'How to Change Your Mind', author: 'Micheal Pollan', isRead:'read'}];
+let myLibrary = [{title: 'The Hobbit', author: 'Tolkien', isRead: 'read' }, {title: 'Meditations', author: 'Marcus Aurelius', isRead: 'read'}, {title: 'How to Change Your Mind', author: 'Micheal Pollan', isRead:'not read'}];
 
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const isRead = document.getElementById('isRead');
-const table = document.getElementById('table');
+const tableBody = document.getElementById('book-table-body');
 const form = document.getElementById('form').addEventListener('submit', (e) =>{
     e.preventDefault();
     addBookToLibrary();
 
 })
 
-class Book {
-    constructor(name, author, isRead){
-        this.name = name;
-        this.author = author;
-        this.isRead = isRead;
-    }
+/* object constructor */
+
+function Book(title, author, isRead) {
+    this.title = title;
+    this.author = author;
+    this.isRead = isRead;
 }
 
 
-/* display function */
 
-function addBookToLibrary() {
-    if (title.value.length === 0 || author.value.length === 0) {
-        alert('Please fill all fields');
-        return;
-    }
-    const newBook = new Book(title.value, author.value, isRead.value);
-    myLibrary.push(newBook);
-}
+
+/* pushes book to the array */
+
+
+
+
