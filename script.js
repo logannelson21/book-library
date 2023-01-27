d20cb074c0b20d00aa7ac424102fd1c4fa94c39f
Logan <logan.nelson012@gmail.com>
2023-01-26 19:06:24 -0800

    Created a new function that pushes the book to the array.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-/* data structures */
-
+/* array for library */
 let myLibrary = [{title: 'The Hobbit', author: 'Tolkien', isRead: 'read' }, {title: 'Meditations', author: 'Marcus Aurelius', isRead: 'read'}, {title: 'How to Change Your Mind', author: 'Micheal Pollan', isRead:'not read'}];
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const isRead = document.getElementById('isRead');
-const tableBody = document.getElementById('book-table-body');
-const form = document.getElementById('form').addEventListener('submit', (e) =>{
-    e.preventDefault();
-    addBookToLibrary();
-
-})
-
 /* object constructor */
-
 function Book(title, author, isRead) {
     this.title = title;
     this.author = author;
     this.isRead = isRead;
 }
 
-
-
-
 /* pushes book to the array */
+function addBookToLibrary(title, author, isRead) {
+    let book = new Book(title, author, isRead);
+    myLibrary.push(book);
+}
+
+/* display books in html table */
+
+
 
 
 
